153c26e0fbd752388a5586d843151e24d888d0af
faridreaming <97384053+faridreaming@users.noreply.github.com>
2025-05-04 22:04:22 +0700

    feat: add dashboard view for admin

---
+++ app/Views/admin/dashboard.php
@@ -0,0 +1,29 @@
+<?= $this->extend('layouts/admin_layout'); ?>
+
+<?= $this->section('content'); ?>
+<div class="card mb-3 dashboard-message">
+    <div class="card-body text-center">
+        <img src="<?= base_url('assets/img/logo.png'); ?>" alt="Logo" class="mb-3">
+        <h5 class="card-title fw-bold">Selamat datang di halaman admin RA Ar-Rayhan</h5>
+        <p class="card-text">Silakan pilih menu di sidebar untuk mengelola data.</p>
+    </div>
+</div>
+
+<div class="row g-3">
+    <?php foreach ($tabel as $nama => $item): ?>
+        <div class="col-12 col-sm-6">
+            <div class="card h-100">
+                <div class="card-body d-flex align-items-center gap-3">
+                    <div class="d-flex bg-primary p-3 text-white rounded fs-3 icon-wrapper">
+                        <i class="bi <?= $item['icon']; ?> d-flex"></i>
+                    </div>
+                    <div>
+                        <h5 class="card-title text-muted small fw-semibold"><?= $nama; ?></h5>
+                        <h6 class="card-subtitle fw-bold fs-3"><?= $item['jumlahData']; ?></h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
+<?= $this->endSection(); ?>
