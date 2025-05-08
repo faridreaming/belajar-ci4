9957f4b60efb7009a30d6fea56277a832cd326bf
Muhammad Farid Yamin <97384053+faridreaming@users.noreply.github.com>
2025-05-08 19:35:21 +0700

    feat: add admin model with authentication methods

--- app/Models/AdminModel.php
+++ app/Models/AdminModel.php
@@ -12,7 +12,7 @@ class AdminModel extends Model
     protected $returnType       = 'object';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['username', 'email', 'password'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
